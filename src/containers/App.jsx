19f2ad8ea2c5d8b9bdb0d51552cea1300bc99cf0
Khanh <tran_khanh@outlook.com>
2020-07-11 07:29:03 +0700

    update

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,80 +1,58 @@
 import React from "react";
-import { Subject, merge } from "rxjs";
-import { map, scan } from "rxjs/operators";
 import Header from "./Header";
-import Main from "./Main";
-import useReactiveState from "./useReactiveState";
+import MainSection from "./MainSection";
+import useReturningState from "./useReturningState";
 
-const todosIntent = initialState => {
-    const addTodo$ = new Subject();
-    const deleteTodo$ = new Subject();
-    const editTodo$ = new Subject();
-    const toggleTodo$ = new Subject();
-    const toggleAllTodo$ = new Subject();
-    const clearCompleted$ = new Subject();
+const initialState = [];
 
-    const todos$ = merge(
-        addTodo$.pipe(
-            map(text => todos => [
-                ...todos,
-                {
-                    id: todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
-                    completed: false,
-                    text
-                }
-            ])
-        ),
-        deleteTodo$.pipe(map(id => todos => todos.filter(todo => todo.id !== id))),
-        editTodo$.pipe(
-            map(({ id, text }) => todos =>
-                todos.map(todo => (todo.id === id ? { ...todo, text } : todo))
-            )
-        ),
-        toggleTodo$.pipe(
-            map(id => todos =>
-                todos.map(
-                    todo =>
-                        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-                )
-            )
-        ),
-        toggleAllTodo$.pipe(
-            map(() => todos => {
-                const areAllMarked = todos.every(todo => todo.completed);
-                return todos.map(todo => ({
-                    ...todo,
-                    completed: !areAllMarked
-                }));
-            })
-        ),
-        clearCompleted$.pipe(
-            map(() => todos => todos.filter(todo => todo.completed === false))
-        )
-    ).pipe(scan((todos, operation) => operation(todos), initialState));
+const addTodo = (text, todos) => [
+    ...todos,
+    {
+        id: todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
+        completed: false,
+        text
+    }
+];
 
-    return {
-        state$: todos$,
-        events$: {
-            addTodo: addTodo$,
-            deleteTodo: deleteTodo$,
-            editTodo: editTodo$,
-            toggleTodo: toggleTodo$,
-            toggleAllTodo: toggleAllTodo$,
-            clearCompleted: clearCompleted$
-        }
-    };
+const deleteTodo = (id, todos) => todos.filter(todo => todo.id !== id);
+
+const editTodo = (id, text, todos) =>
+    todos.map(todo => (todo.id === id ? { ...todo, text } : todo));
+
+const toggleTodo = (id, todos) =>
+    todos.map(
+        todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)
+    );
+
+const toggleAllTodo = todos => {
+    const areAllMarked = todos.every(todo => todo.completed);
+    return todos.map(todo => ({
+        ...todo,
+        completed: !areAllMarked
+    }));
+};
+
+const clearCompleted = todos => todos.filter(todo => todo.completed === false);
+
+const actions = {
+    addTodo,
+    deleteTodo,
+    editTodo,
+    toggleTodo,
+    toggleAllTodo,
+    clearCompleted
 };
 
 const App = () => {
     const [
         todos,
         { addTodo, deleteTodo, editTodo, toggleTodo, toggleAllTodo, clearCompleted }
-    ] = useReactiveState(todosIntent, []);
+    ] = useReturningState(initialState, actions);
 
     return (
         <div>
             <Header addTodo={addTodo} />
-            <Main
+            <MainSection
                 todos={todos}
                 deleteTodo={deleteTodo}
                 editTodo={editTodo}
